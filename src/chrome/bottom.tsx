8019049831736c440febd16f4b326756326f33da
Banou <me@banou.dev>
2022-08-16 05:30:52 +0200

    feat: ✨player control tooltips

--- src/chrome/bottom.tsx
+++ src/chrome/bottom.tsx
@@ -11,6 +11,7 @@ import { Volume, Volume1, Volume2, VolumeX, AlertTriangle } from 'react-feather'
 import useScrub from '../use-scrub'
 import { Subtitle } from '../worker'
 import { tagToLanguage } from '../languages'
+import ReactTooltip from 'react-tooltip'
 
 const style = css`
   position: relative;
@@ -21,7 +22,6 @@ const style = css`
   width: 100%;
   padding: 0 2rem;
   margin: 0 auto;
-
 
   /* subtle background black gradient for scenes with high brightness to properly display white progress bar */
   padding-top: var(--background-padding);
@@ -67,19 +67,7 @@ const style = css`
     color: #fff;
     user-select: none;
 
-    .picture-in-picture {
-      display: grid;
-      justify-items: center;
-      cursor: pointer;
-    }
-
-    .fullscreen {
-      display: grid;
-      justify-items: center;
-      cursor: pointer;
-    }
-
-    .play-button {
+    .picture-in-picture, .fullscreen, .play-button {
       height: 100%;
       color: #fff;
       background-color: transparent;
@@ -434,7 +422,16 @@ export default ({
         <div className="padding" onMouseDown={seekScrub}></div>
       </div>
       <div className="controls">
-        <button className="play-button" type="button" onClick={clickPlay}>
+        <ReactTooltip id='play-button-tooltip' effect='solid' place='top'>
+          <span>
+            {
+              isPlaying
+                ? 'Pause (k)'
+                : 'Play (k)'
+            }
+          </span>
+        </ReactTooltip>
+        <button className="play-button" type="button" data-tip data-for='play-button-tooltip' onClick={clickPlay}>
           {
             isPlaying
               ? <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-pause"><rect x="6" y="4" width="4" height="16"></rect><rect x="14" y="4" width="4" height="16"></rect></svg>
@@ -442,7 +439,16 @@ export default ({
           }
         </button>
         <div className="volume-area" onMouseOver={hoverVolumeArea}>
-          <button className="mute-button" onClick={clickMuteButton}>
+          <ReactTooltip id='mute-button-tooltip' effect='solid' place='top'>
+            <span>
+              {
+                isMuted
+                  ? 'Unmute (m)'
+                  : 'Mute (m)'
+              }
+            </span>
+          </ReactTooltip>
+          <button className="mute-button" data-tip data-for='mute-button-tooltip' onClick={clickMuteButton}>
             {
               isMuted ? <VolumeX/>
               : (volumeScrubValue ?? 0) > 0.66 ? <Volume2/>
@@ -490,6 +496,9 @@ export default ({
               : null
           }
         </div>
+        <ReactTooltip id='subtitle-button-tooltip' effect='solid' place='top'>
+          <span>Subtitles</span>
+        </ReactTooltip>
         <div className="subtitle-area">
           {
             tracks.length
@@ -508,7 +517,7 @@ export default ({
                     )
                   }
                 </div>
-                <button className="subtitle-menu-button" onClick={subtitleMenuButtonClick}>
+                <button className="subtitle-menu-button" data-tip data-for='subtitle-button-tooltip' onClick={subtitleMenuButtonClick}>
                   <SubtitleTrackToName subtitleTrack={subtitleTrack}/>
                 </button>
               </>
@@ -519,19 +528,28 @@ export default ({
         {
           isPictureInPictureEnabled
             ? (
-              <div className="picture-in-picture" onClick={pictureInPicture}>
+              <button className="picture-in-picture" onClick={pictureInPicture}>
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><rect x="2" y="3" width="21" height="14" rx="2" ry="2"></rect><rect x="12.5" y="8.5" width="8" height="6" rx="2" ry="2"></rect></svg>
-              </div>
+              </button>
             )
             : null
         }
-        <div className="fullscreen" onClick={clickFullscreen}>
+        <ReactTooltip id='fullscreen-button-tooltip' effect='solid' place='top'>
+          <span>
+            {
+              isFullscreen
+                ? 'Exit full screen (f)'
+                : 'Full screen (f)'
+            }
+          </span>
+        </ReactTooltip>
+        <button className="fullscreen" data-tip data-for='fullscreen-button-tooltip' onClick={clickFullscreen}>
           {
             isFullscreen
               ? <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-minimize"><path d="M8 3v3a2 2 0 0 1-2 2H3m18 0h-3a2 2 0 0 1-2-2V3m0 18v-3a2 2 0 0 1 2-2h3M3 16h3a2 2 0 0 1 2 2v3"></path></svg>
               : <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-maximize"><path d="M8 3H5a2 2 0 0 0-2 2v3m18 0V5a2 2 0 0 0-2-2h-3m0 18h3a2 2 0 0 0 2-2v-3M3 16v3a2 2 0 0 0 2 2h3"></path></svg>
           }
-        </div>
+        </button>
       </div>
     </div>
   )
