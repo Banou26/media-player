78c917a7c5ec6c0444cb53dfe9936f23044c1a24
Banou <me@banou.dev>
2023-02-21 03:15:14 +0100

    fix: 🐛bufferStream do not swallow errors

--- src/utils.ts
+++ src/utils.ts
@@ -76,6 +76,7 @@ export const queuedDebounceWithLastCall = <T2 extends any[], T extends (...args:
 }
 
 // todo: reimplement this into a ReadableByteStream https://web.dev/streams/ once FF gets support
+// todo: remove firefox workaround once https://bugzilla.mozilla.org/show_bug.cgi?id=1808868 ships
 export const bufferStream = ({ stream, size: SIZE }: { stream: ReadableStream, size: number }) =>
   new ReadableStream<Uint8Array>({
     start() {
@@ -83,40 +84,47 @@ export const bufferStream = ({ stream, size: SIZE }: { stream: ReadableStream, s
       this.reader = stream.getReader()
     },
     async pull(controller) {
-      // @ts-expect-error
-      const { leftOverData }: { leftOverData: Uint8Array | undefined } = this
-
-      const accumulate = async ({ buffer = new Uint8Array(SIZE), currentSize = 0 } = {}): Promise<{ buffer?: Uint8Array, currentSize?: number, done: boolean }> => {
+      try {
         // @ts-expect-error
-        const { value: newBuffer, done } = await this.reader.read()
-  
-        if (currentSize === 0 && leftOverData) {
-          buffer.set(leftOverData)
-          currentSize += leftOverData.byteLength
+        const { leftOverData }: { leftOverData: Uint8Array | undefined } = this
+
+        const accumulate = async ({ buffer = new Uint8Array(SIZE), currentSize = 0 } = {}): Promise<{ buffer?: Uint8Array, currentSize?: number, done: boolean }> => {
           // @ts-expect-error
-          this.leftOverData = undefined
+          const { value: newBuffer, done } = await this.reader.read()
+
+          if (currentSize === 0 && leftOverData) {
+            console.log('currentSize', currentSize)
+            console.log('buffer', buffer)
+            console.log('leftOverData', leftOverData)
+            buffer.set(leftOverData)
+            currentSize += leftOverData.byteLength
+            // @ts-expect-error
+            this.leftOverData = undefined
+          }
+    
+          if (done) {
+            return { buffer: buffer.slice(0, currentSize), currentSize, done }
+          }
+    
+          let newSize
+          const slicedBuffer = newBuffer.slice(0, SIZE - currentSize)
+          newSize = currentSize + slicedBuffer.byteLength
+          buffer.set(slicedBuffer, currentSize)
+    
+          if (newSize === SIZE) {
+            // @ts-expect-error
+            this.leftOverData = newBuffer.slice(SIZE - currentSize)
+            return { buffer, currentSize: newSize, done: false }
+          }
+          
+          return accumulate({ buffer, currentSize: newSize })
         }
-  
-        if (done) {
-          return { buffer: buffer.slice(0, currentSize), currentSize, done }
-        }
-  
-        let newSize
-        const slicedBuffer = newBuffer.slice(0, SIZE - currentSize)
-        newSize = currentSize + slicedBuffer.byteLength
-        buffer.set(slicedBuffer, currentSize)
-  
-        if (newSize === SIZE) {
-          // @ts-expect-error
-          this.leftOverData = newBuffer.slice(SIZE - currentSize)
-          return { buffer, currentSize: newSize, done: false }
-        }
-        
-        return accumulate({ buffer, currentSize: newSize })
+        const { buffer, done } = await accumulate()
+        if (buffer?.byteLength) controller.enqueue(buffer)
+        if (done) controller.close()
+      } catch (err) {
+        console.error(err)
       }
-      const { buffer, done } = await accumulate()
-      if (buffer?.byteLength) controller.enqueue(buffer)
-      if (done) controller.close()
     },
     cancel() {
       // @ts-expect-error
