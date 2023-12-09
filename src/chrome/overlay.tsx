1adb1f7d5b2c0bd1db157c0ce328c4d847e8b5a6
Banou <me@banou.dev>
2023-12-09 21:02:43 +0100

    feat: ✨improve initial interaction UI

--- src/chrome/overlay.tsx
+++ src/chrome/overlay.tsx
@@ -46,10 +46,10 @@ export default ({ loading, needsInitialInteraction, togglePlay, setCanvasRef }: 
           ? (
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              width="100"
-              height="100"
-              viewBox="0 0 100 100"
-              fill="none"
+              width="60"
+              height="60"
+              viewBox="0 0 60 60"
+              fill="black"
               stroke="currentColor"
               strokeWidth="3"
               strokeLinecap="round"
